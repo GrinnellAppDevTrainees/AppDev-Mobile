efb4f149a71c38e3275d03a49b94690c7947c8a0
albertowusuasare <owusuasa@grinnell.edu>
2014-10-23 20:15:37 -0500

    Implemented swipeable tabs in ActivityFeed activity Populated ListView in Article tab with dummy contents Implemented fragment in RSS tab Implemented fragment in SocialMedia tab

--- AppDevDbProject/gen/com/example/appdevdbproject/R.java
+++ AppDevDbProject/gen/com/example/appdevdbproject/R.java
@@ -858,10 +858,14 @@ containing a value of this type.
         public static final int abc_split_action_bar_is_narrow=0x7f060002;
     }
     public static final class color {
-        public static final int abc_search_url_text_holo=0x7f070003;
+        public static final int abc_search_url_text_holo=0x7f070005;
         public static final int abc_search_url_text_normal=0x7f070000;
         public static final int abc_search_url_text_pressed=0x7f070002;
         public static final int abc_search_url_text_selected=0x7f070001;
+        public static final int grey=0x7f070004;
+        /**  Feel free to play with the values here 
+         */
+        public static final int light_grey=0x7f070003;
     }
     public static final class dimen {
         /**  Default height of an action bar. 
@@ -1080,10 +1084,10 @@ containing a value of this type.
         public static final int ic_launcher=0x7f020057;
     }
     public static final class id {
-        public static final int About=0x7f05003f;
-        public static final int ActivityFeed=0x7f05003d;
-        public static final int Directory=0x7f05003e;
-        public static final int Services=0x7f05003c;
+        public static final int About=0x7f050040;
+        public static final int ActivityFeed=0x7f05003e;
+        public static final int Directory=0x7f05003f;
+        public static final int Services=0x7f05003d;
         public static final int action_bar=0x7f05001c;
         public static final int action_bar_activity_content=0x7f050015;
         public static final int action_bar_container=0x7f05001b;
@@ -1095,13 +1099,14 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050040;
+        public static final int action_settings=0x7f050043;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int default_activity_button=0x7f050028;
+        public static final int details=0x7f050042;
         public static final int dialog=0x7f05000e;
         public static final int disableHome=0x7f050008;
         public static final int dropdown=0x7f05000f;
@@ -1120,6 +1125,7 @@ containing a value of this type.
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
+        public static final int pager=0x7f05003c;
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
@@ -1141,6 +1147,7 @@ containing a value of this type.
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
         public static final int title=0x7f05002b;
+        public static final int titles=0x7f050041;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
         public static final int useLogo=0x7f050003;
@@ -1201,7 +1208,10 @@ containing a value of this type.
         public static final int activity_directory=0x7f03001a;
         public static final int activity_home=0x7f03001b;
         public static final int activity_services=0x7f03001c;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001d;
+        public static final int fragment_article=0x7f03001d;
+        public static final int fragment_rss=0x7f03001e;
+        public static final int fragment_social_media=0x7f03001f;
+        public static final int support_simple_spinner_dropdown_item=0x7f030020;
     }
     public static final class menu {
         public static final int about=0x7f0c0000;
--- AppDevDbProject/src/activityFeed/ActivityFeedActivity.java
+++ AppDevDbProject/src/activityFeed/ActivityFeedActivity.java
@@ -1,29 +1,78 @@
 package activityFeed;
 
-import com.example.appdevdbproject.R;
-import com.example.appdevdbproject.R.id;
-import com.example.appdevdbproject.R.layout;
-import com.example.appdevdbproject.R.menu;
-
-import android.support.v7.app.ActionBarActivity;
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class ActivityFeedActivity extends ActionBarActivity {
+import com.example.appdevdbproject.R;
+
+public class ActivityFeedActivity extends FragmentActivity implements
+		ActionBar.TabListener {
+
+	// Variable types created from tab classes to initialize an actionBar, a tab
+	// menu in the actionBar,
+	// and a base for swipe-able views
+	private ActionBar actionBar;
+	private TabsAdapter mAdapter;
+	private ViewPager viewPager;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_activity_feed);
-	}
+
+		// Initialization for an actionBar, a tab menu, and a base for
+		// swipe-able views
+		viewPager = (ViewPager) findViewById(R.id.pager);
+		actionBar = getActionBar();
+		mAdapter = new TabsAdapter(getSupportFragmentManager());
+		// Setting adapter for ViewPager, setting navigation mode of actionBar
+		// to 'Tabs'
+		viewPager.setAdapter(mAdapter);
+		actionBar.setHomeButtonEnabled(false);
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+		// set Text of the Tabs with respective names
+		for (String tab_name : Utilities.tabs) {
+			actionBar.addTab(actionBar.newTab().setText(tab_name)
+					.setTabListener(this));
+		}
+
+		viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+			/**
+			 * on swiping the <code>viewPager</code> creates respective tab
+			 * selected
+			 **/
+
+			@Override
+			public void onPageSelected(int position) {
+				// on changing the page
+				// make respected tab selected
+				actionBar.setSelectedNavigationItem(position);
+			}
+
+			@Override
+			public void onPageScrolled(int arg0, float arg1, int arg2) {
+			}
+
+			@Override
+			public void onPageScrollStateChanged(int arg0) {
+			}
+		});
+	}//onCreate
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_feed, menu);
 		return true;
-	}
+	}//onCreateOptionsMenu
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
@@ -35,5 +84,25 @@ public class ActivityFeedActivity extends ActionBarActivity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
-	}
-}
+	}//onOptionsItemSelected
+
+	@Override
+	public void onTabSelected(Tab tab, FragmentTransaction ft) {
+		// For the tab that we are currently on, the actionBar will show the
+		// appropriate
+		// tab selected.
+		viewPager.setCurrentItem(tab.getPosition());
+	}//onTabSelected
+
+	@Override
+	public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+
+	}//onTabUnselected
+
+	@Override
+	public void onTabReselected(Tab tab, FragmentTransaction ft) {
+		// TODO Auto-generated method stub
+
+	}//onTabReselected
+}//ActivityFeedActivity
